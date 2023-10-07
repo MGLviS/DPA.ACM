1c9513f4d91d1f9616b810b78c46dfdb6874afe9
Jonathan <jvasquezqm@gmail.com>
2023-10-07 17:19:45 -0500

    vehiculo ct

---
+++ DPA.ACM.API/Controllers/VehiculoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DPA.ACM.API.Controllers
+{
+    public class VehiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
